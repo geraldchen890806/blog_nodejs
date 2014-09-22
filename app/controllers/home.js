a11543b725ff475430d559e0f70e29ba3f556f7b
geraldchen890806 <geraldchen890806@gmail.com>
2014-09-22 15:37:44 +0800

    add blog list

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,20 @@
 /**
  * home
 **/
+var md = require("marked");
+var blogDB = require("../models/blog").db;
 
-exports.index = function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var blogs = [{'title': 'test1', 'content': 'testttestttestttestttestttestttestttestttestttestttestttestttestttestttestttestt','tag':'test','addTime': '2014:09-22 11:11:11'},
+             {'title': 'test2', 'content': 'testttestttestttestttestttestttestttestttestttestttestttestttestttestttestttestt','tag':'test','addTime': '2014:09-22 11:11:11'},
+             {'title': 'test3', 'content': 'testttestttestttestttestttestttestttestttestttestttestttestttestttestttestttestt','tag':'test','addTime': '2014:09-22 11:11:11'},
+            ]
+
+exports.index = function (req, res, next) {
+  blogDB.getList(function (err, data) {
+    data.map(function(v, i){
+      v.content = md(v.content)
+      return v;
+    })
+    res.render('index', { blogs: data });
+  })
 }
