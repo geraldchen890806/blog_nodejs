8a66a6d698bb1224128bf8d8b512a88c5267796d
geraldchen890806 <geraldchen890806@gmail.com>
2014-10-08 17:02:32 +0800

    error url

--- app/controllers/blogs.js
+++ app/controllers/blogs.js
@@ -5,9 +5,11 @@ var tagDB = require("../models/tag").db;
 exports.index = function *() {
   var id = this.url.replace(/^\/blog\//,"");
   var result = yield blogDB.findByID(id);
-  if(this.ip !="127.0.0.1") {
+
+  if(this.ip !="127.0.0.1" && result && result.length) {
     yield blogDB.queryStr("update `blogs` set `times`="+ (parseInt(result.times) + 1) +" where `id`=" + id)
   }
+
   var recentBlogs = yield blogDB.getRecentBlogs();
   var tags = yield tagDB.getTags();
   yield this.render('blogs/blog', { blog: result, recentBlogs: recentBlogs, tags: tags});
