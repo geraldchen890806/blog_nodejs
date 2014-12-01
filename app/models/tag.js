dfb9a81e3608ef6feaf007116de6babf61c98092
geraldchen <geraldchen890806@gmail.com>
2014-12-01 14:22:30 +0800

    spilt tag db fn

--- app/models/tag.js
+++ app/models/tag.js
@@ -1,18 +1,12 @@
 var db = require("./db").db("tags");
 
 
-db.getTags = function *() {
-  if (!this.tags) {
-    this.tags = yield this.getList();
-  }
+db.sqlTags = function *() {
+  this.tags = yield this.getList();
   return this.tags;
 }
 
-db.save = function *(tags) {
-  var res = yield this.queryStr("insert into blog_tag values ?", [tags]);
-  console.log("res", res);
-  if(res.insertId) return true;
-  return false;
+db.getTags = function *() {
+  return this.tags || this.sqlTags();
 }
-
 exports.db = db;
