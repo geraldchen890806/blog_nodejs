0bbe7be5454d7471f60c6522aaed84ee881b9431
geraldchen <geraldchen890806@gmail.com>
2015-01-23 16:28:39 +0800

    fix home layout path

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -8,18 +8,18 @@ var blogDB = require("../models/blog").db,
 exports.index = function *() {
   var blogs = yield blogDB.getBlogs();
   var commonConfig = yield common.config();
-  yield this.render('blogs/index', extend({ blogs: blogs}, commonConfig, {session: this.session}));
+  yield this.render('home/index', extend({ blogs: blogs}, commonConfig, {session: this.session}));
 };
 
 exports.recommend = function *() {
   var blogs = yield blogDB.getRecommend();
   var commonConfig = yield common.config();
-  yield this.render('blogs/index', extend({ blogs: blogs}, commonConfig, {session: this.session}));
+  yield this.render('home/index', extend({ blogs: blogs}, commonConfig, {session: this.session}));
 };
 
 exports.about = function *() {
   var url = gravatar.url('geraldchen890806@gmail.com', {s: '200', r: 'pg', d: '404'});
-  yield this.render('shares/about', {imageUrl: url, session: this.session});
+  yield this.render('home/about', {imageUrl: url, session: this.session});
 };
 
 exports.feed = function *() {
