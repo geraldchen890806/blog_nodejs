dd30a36b8524a5f9839461111d909eabb0bbb260
陈广亮 <guangliang.chen@DatayesdeMacBook-Pro.local>
2015-05-25 15:44:53 +0800

    remove google analysis

--- app/controllers/common.js
+++ app/controllers/common.js
@@ -4,25 +4,33 @@ var blogDB = require("../models/blog").db,
     md = require("marked"),
     config = require("../../config").config;
 
-exports.config = function *() {
-  var recentBlogs = yield blogDB.getRecentBlogs();
-  var tags = yield tagDB.getTags();
-  var keys = [].concat(config.keys);
-  tags.forEach(function(v, i) {
-    keys.push(v.name);
-  });
-  return {recentBlogs: recentBlogs, tags: tags, title: "", keys: keys.join(",")}
+exports.config = function*() {
+    var recentBlogs = yield blogDB.getRecentBlogs();
+    var tags = yield tagDB.getTags();
+    var keys = [].concat(config.keys);
+    // tags.forEach(function(v, i) {
+    //   keys.push(v.name);
+    // });
+    return {
+        recentBlogs: recentBlogs,
+        tags: tags,
+        title: "",
+        keys: keys.join(",")
+    }
 };
 
-exports.editor = function *() {
-  var body = yield parse(this);
-  this.body = md(body.data);
+exports.editor = function*() {
+    var body = yield parse(this);
+    this.body = md(body.data);
 };
 
-exports.checkPermission = function *(session, next) {
-  if(session.login) {
-    session.err = {status:500, message: "you have no permission123"};
-    return false;
-  }
-  return true;
+exports.checkPermission = function*(session, next) {
+    if (session.login) {
+        session.err = {
+            status: 500,
+            message: "you have no permission123"
+        };
+        return false;
+    }
+    return true;
 };
