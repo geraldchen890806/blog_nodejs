182f5883aad5eceea469206b39071c58487f28c0
陈广亮 <guangliang.chen@DatayesdeMacBook-Pro.local>
2015-05-25 16:28:07 +0800

    redirect to www.renjm.com

--- app.js
+++ app.js
@@ -21,21 +21,21 @@ app.use(gzip());
 // view engine setup
 app.keys = ['keys', 'geraldblog'];
 app.use(jade.middleware({
-  viewPath: __dirname + '/app/views',
-  //debug: false,
-  //pretty: false,
-  //compileDebug: false,
-  //locals: global_locals_for_all_pages,
-  basedir: 'path/for/jade/extends'
-  //helperPath: [
-  //  'path/to/jade/helpers',
-  //  { random: 'path/to/lib.js' },
-  //  { _: require('lodash') }
-  //]
+    viewPath: __dirname + '/app/views',
+    //debug: false,
+    //pretty: false,
+    //compileDebug: false,
+    //locals: global_locals_for_all_pages,
+    basedir: 'path/for/jade/extends'
+        //helperPath: [
+        //  'path/to/jade/helpers',
+        //  { random: 'path/to/lib.js' },
+        //  { _: require('lodash') }
+        //]
 }));
 
-app.use(staticCache(path.join(__dirname, 'public'),{
-  maxAge: 365 * 24 * 60 * 60
+app.use(staticCache(path.join(__dirname, 'public'), {
+    maxAge: 365 * 24 * 60 * 60
 }));
 
 var favicon = require('koa-favicon');
@@ -43,11 +43,11 @@ var favicon = require('koa-favicon');
 app.use(favicon(__dirname + '/favicon.ico'));
 
 app.use(session({
-  store: new MysqlStore(config.db),
-  rolling: true,
-  cookie: {
-    maxage: 30 * 24 * 60 * 60 * 1000
-  }
+    store: new MysqlStore(config.db),
+    rolling: true,
+    cookie: {
+        maxage: 30 * 24 * 60 * 60 * 1000
+    }
 }));
 
 // app.use(serve('public', {
@@ -62,6 +62,14 @@ app.use(session({
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function*(next) {
+    if (this.hostname == "renjm.com") {
+        var herf = this.request.href;
+        this.redirect(href.replace("renjm.com", "www.renjm.com"));
+    }
+    yield next;
+});
 
 route(app);
 
