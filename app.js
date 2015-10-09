15c10710ba4669ec404a90a5202adb2ce3002913
geraldchen <geraldchen890806@gmail.com>
2015-10-09 15:00:52 +0800

    fix

--- app.js
+++ app.js
@@ -6,12 +6,8 @@ var path = require('path');
 var staticCache = require('koa-static-cache');
 var config = require("./config");
 var session = require('koa-generic-session');
-var MysqlStore = require('koa-mysql-session');
+// var MysqlStore = require('koa-mysql-session');
 var gzip = require("koa-gzip");
-// var path = require('path');
-// var favicon = require('serve-favicon');
-// var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
 
 var route = require('./routes');
 var app = koa();
@@ -22,16 +18,7 @@ app.use(gzip());
 app.keys = ['keys', 'geraldblog'];
 app.use(jade.middleware({
     viewPath: __dirname + '/app/views',
-    //debug: false,
-    //pretty: false,
-    //compileDebug: false,
-    //locals: global_locals_for_all_pages,
     basedir: 'path/for/jade/extends'
-        //helperPath: [
-        //  'path/to/jade/helpers',
-        //  { random: 'path/to/lib.js' },
-        //  { _: require('lodash') }
-        //]
 }));
 
 app.use(staticCache(path.join(__dirname, 'public'), {
@@ -43,25 +30,11 @@ var favicon = require('koa-favicon');
 app.use(favicon(__dirname + '/favicon.ico'));
 
 app.use(session({
-    store: new MysqlStore(config.db),
     rolling: true,
     cookie: {
         maxage: 30 * 24 * 60 * 60 * 1000
     }
 }));
-
-// app.use(serve('public', {
-//   maxage: 3650000000,
-//   Etag: 1231231231
-// }));
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function*(next) {
     if (this.hostname == "renjm.com") {
@@ -72,7 +45,5 @@ app.use(function*(next) {
 });
 
 route(app);
-
-// error handlers
 
 module.exports = app;
